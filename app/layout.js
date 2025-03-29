bab87b7438143c17f97c9494140288d07f79348f
zbytes2227 <zbytes0@gmail.com>
2025-03-29 16:13:17 +0530

    fixes ad

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,20 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Under from "@/components/Under";
+import Navbar from "@/components/Navbar"; 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "BharatGen Smart Attendance",
-  description: "Smart Attendance",
+  title: "Smart Mess Attendance",
+  description: "Mess Attendance",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-          {/* <Navbar/> */}
-        {/* <Under/> */}
-       {/* {children} */}
-    <p>UNDER MAINTENANCE</p>
+          <Navbar/>   
+         {children} 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js" defer  />
       </body>
     </html>
